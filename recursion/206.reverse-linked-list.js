03c5c1a99d17e755a57d88556a67e1f8a6f0be18
alaaTharwat <alaa.tharrwat@gmail.com>
2022-05-05 16:51:12 +0200

    new

--- recursion/206.reverse-linked-list.js
+++ recursion/206.reverse-linked-list.js
@@ -22,6 +22,21 @@ var reverseList = function(head, pre = null) {
     const next = head.next;
     head.next = pre;
     return reverseList(next, head)
+
+    // let curr = head; //cursor to the current node
+    // let prev = null; //cursor to the previous node
+  
+    // while (curr) { //curr !== null
+    //   let tmp = curr.next; //move cursor to the next node
+  
+    //   curr.next = prev; //1.change curr pointer backwards
+    //   prev = curr; //2.change prev cursor to current
+    //   curr = tmp; //3.move cur cursor to the next node
+    //   console.log("prev",prev)
+    //   console.log("curr",curr)
+
+    // }
+    // return prev; //return a new head of the list
 };
 // @lc code=end
 
