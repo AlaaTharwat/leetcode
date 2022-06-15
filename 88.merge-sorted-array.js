d41b4dc5a6a33bdad7ef85fc89f05d4417563228
alaaTharwat <alaa.tharrwat@gmail.com>
2022-06-15 13:24:54 +0200

    _

---
+++ 88.merge-sorted-array.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let size = m + n - 1;
+    let i = m -1 ;
+    let j = n -1 ;
+    while(size >= 0){
+        if (j >= 0 && i >= 0) {
+            if(nums1[i] >nums2[j]){
+                nums1[size--] = nums1[i--];
+            }else{
+                nums1[size--] = nums2[j--];
+            }
+        }else if(i >= 0){
+            nums1[size--] = nums1[i--];
+        }else{
+            nums1[size--] = nums2[j--]
+        }
+    }
+
+};
+// @lc code=end
+
